fe66392bf7a3debd8975bc008c154a30349a0ee8
sbhongade598@gmail.com <sbhongade598@gmail.com>
2022-11-18 12:38:01 +0900

    intial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="home">
+      <h1>learn slot</h1>
+    </div>
+    
+  </main>
+</template>
+
+<style scoped>
+ .home h1{
+  min-height: 100vh;
+ }
+
+</style>
